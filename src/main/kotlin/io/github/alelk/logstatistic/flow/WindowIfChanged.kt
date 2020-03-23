95c84c0c1fddd8417750d2a5a4bdeee90576cc63
Alex Elkin <alelkdev@gmail.com>
2020-03-23 07:11:23 +0300

    refactor(window-if-changed): Refactor 'WindowIfChanged'.

--- src/main/kotlin/io/github/alelk/logstatistic/flow/WindowIfChanged.kt
+++ src/main/kotlin/io/github/alelk/logstatistic/flow/WindowIfChanged.kt
@@ -6,36 +6,30 @@ import io.reactivex.FlowableTransformer
 import io.reactivex.functions.BiFunction
 import java.util.concurrent.Callable
 
-fun <A, B> windowIfChanged(keySelector: (A) -> B): FlowableTransformer<A, List<A>> = FlowableTransformer<A, List<A>> { f ->
+fun <A, B> windowIfChanged(keySelector: (A) -> B) = FlowableTransformer<A, List<A>> { f ->
     val iterator = f.blockingIterable(1).iterator()
-    if (iterator.hasNext()) {
-        var firstInWindow = iterator.next()
-        Flowable.generate(
-                Callable {Unit},
-                BiFunction { _, emitter: Emitter<List<A>> ->
-                    when {
-                        iterator.hasNext() -> {
-                            val key = keySelector(firstInWindow)
-                            val items = mutableListOf(firstInWindow)
-                            while (iterator.hasNext()) {
-                                val next = iterator.next()
-                                val nextKey = keySelector(next)
-                                if (key == nextKey) items.add(next)
-                                else {
-                                    firstInWindow = next
-                                    break
-                                }
-                            }
-                            emitter.onNext(items)
-                        }
-                        else -> {
-                            emitter.onNext(listOf(firstInWindow))
-                            emitter.onComplete()
+    Flowable.generate(
+            Callable { ((null as B?) to emptyList<A>()) },
+            BiFunction { (currentKey: B?, currentWindow: List<A>), emitter: Emitter<List<A>> ->
+                when {
+                    iterator.hasNext() -> {
+                        val next = iterator.next()
+                        val nextKey = keySelector(next)
+                        if (currentKey == nextKey) {
+                            currentKey to (currentWindow + next)
+                        } else {
+                            if (currentWindow.isNotEmpty()) emitter.onNext(currentWindow)
+                            nextKey to listOf(next)
                         }
                     }
+                    else -> {
+                        if (currentWindow.isNotEmpty()) emitter.onNext(currentWindow)
+                        emitter.onComplete()
+                        null to emptyList()
+                    }
                 }
-        )
-    } else Flowable.empty<List<A>>()
+            }
+    )
 }
 
 fun main() {
